4aa281aaecb3b93a30f4f16c17accc7eda513ff7
OthmanCharai <otmanchara3i0688@gmail.com>
2022-10-09 21:43:45 +0100

    clean code

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -11,9 +11,7 @@
     <title>HAKDER &#8211; federatie</title>
     <script src="https://code.jquery.com/jquery-3.6.1.slim.js" integrity="sha256-tXm+sa1uzsbFnbXt8GJqsgi2Tw+m4BLGDof6eUPjbtk=" crossorigin="anonymous"></script>
     <!-- BEGIN: CSS Assets-->
-     <link rel="stylesheet" href="{{ asset('app.css') }}">
-     
-    <script src="{{ asset('build/assets/app.js') }}"></script>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
     <!-- END: CSS Assets-->
 </head>
 <!-- END: Head -->
@@ -40,47 +38,47 @@
         @role('admin')
         <li >
             <a href="{{ route('home') }}"  class="side-menu side-menu--{{ request()->is('dashboard') ? 'active' : '' }}">
-                <div class="side-menu__icon"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg></div>
+                <div class="side-menu__icon"> <i data-feather="home"></i> </div>
                 <div class="side-menu__title"> Dashboard </div>
             </a>
         </li>
         
         <li>
             <a href="{{ route('plan.index') }}" class="side-menu {{ request()->is('plan') ? ' side-menu--active' : '' }}">
-                <div class="side-menu__icon"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-navigation"><polygon points="3 11 22 2 13 21 11 13 3 11"></polygon></svg> </div>
+                <div class="side-menu__icon"> <i data-feather="navigation"></i> </div>
                 <div class="side-menu__title">Plans  </div>
             </a>
         </li>
         <li>
             <a href="{{ route('user.unimproved') }}" class="side-menu side-menu--{{ request()->is('users/unimproved') ? 'active' : '' }}">
-                <div class="side-menu__icon"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user-x"><path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="8.5" cy="7" r="4"></circle><line x1="18" y1="8" x2="23" y2="13"></line><line x1="23" y1="8" x2="18" y2="13"></line></svg> </div>
+                <div class="side-menu__icon"> <i data-feather="user-x"></i> </div>
                 <div class="side-menu__title"> Un Approved Members  </div>
             </a>
         </li>
         <li>
             <a href="{{ route('user.index') }}" class="side-menu side-menu--{{ request()->is('user') ? 'active' : '' }}">
-                <div class="side-menu__icon"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user-check"><path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="8.5" cy="7" r="4"></circle><polyline points="17 11 19 13 23 9"></polyline></svg> </div>
+                <div class="side-menu__icon"> <i data-feather="user-check"></i> </div>
                 <div class="side-menu__title">Members  </div>
             </a>
         </li>
      
         <li>
             <a href="{{ route('invoice.index') }}" class="side-menu side-menu--{{ request()->is('invoice') ? 'active' : '' }}">
-                <div class="side-menu__icon"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-plus"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="12" y1="18" x2="12" y2="12"></line><line x1="9" y1="15" x2="15" y2="15"></line></svg> </div>
+                <div class="side-menu__icon"> <i data-feather="file-plus"></i> </div>
                 <div class="side-menu__title"> Invoices  </div>
             </a>
         </li>
 
         <li>
             <a href="{{ route('child.index') }}" class="side-menu side-menu--{{ request()->is('child') ? 'active' : '' }}">
-                <div class="side-menu__icon"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-plus"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="12" y1="18" x2="12" y2="12"></line><line x1="9" y1="15" x2="15" y2="15"></line></svg></div>
+                <div class="side-menu__icon"> <i data-feather="file-plus"></i> </div>
                 <div class="side-menu__title"> Children  </div>
             </a>
         </li>
      
         <li>
             <a href="{{ route('importe') }}" class="side-menu side-menu--{{ request()->is('importes') ? 'active' : '' }}">
-                <div class="side-menu__icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-anchor"><circle cx="12" cy="5" r="3"></circle><line x1="12" y1="22" x2="12" y2="8"></line><path d="M5 12H2a10 10 0 0 0 20 0h-3"></path></svg></div>
+                <div class="side-menu__icon"> <i data-feather="anchor"></i> </div>
                 <div class="side-menu__title">Import  </div>
             </a>
         </li>
@@ -88,19 +86,19 @@
         @role('member')
         <li>
             <a href="{{ route('member.dashboard') }}" class="side-menu side-menu--{{ request()->is('member/dashboard') ? 'active' : '' }}">
-                <div class="side-menu__icon"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-home"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg></div>
+                <div class="side-menu__icon"> <i data-feather="anchor"></i> </div>
                 <div class="side-menu__title">Dahboard  </div>
             </a>
         </li>
         <li>
             <a href="{{ route('invoice.index') }}" class="side-menu side-menu--{{ request()->is('invoice') ? 'active' : '' }}">
-                <div class="side-menu__icon"> svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-file-plus"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="12" y1="18" x2="12" y2="12"></line><line x1="9" y1="15" x2="15" y2="15"></line></svg></div>
+                <div class="side-menu__icon"> <i data-feather="file-plus"></i> </div>
                 <div class="side-menu__title"> Invoices  </div>
             </a>
         </li>
         <li>
             <a href="{{ route('profile') }}" class="side-menu side-menu--{{ request()->is('profile') ? 'active' : '' }}">
-                <div class="side-menu__icon"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-user-check"><path d="M16 21v-2a4 4 0 0 0-4-4H5a4 4 0 0 0-4 4v2"></path><circle cx="8.5" cy="7" r="4"></circle><polyline points="17 11 19 13 23 9"></polyline></svg></div>
+                <div class="side-menu__icon"> <i data-feather="user"></i> </div>
                 <div class="side-menu__title">Profile  </div>
             </a>
         </li>
@@ -108,7 +106,7 @@
         @endrole
         <li>
             <a href="{{ route('logout') }}" class="side-menu" onclick="event.preventDefault();  document.getElementById('logout-form').submit();">
-                <div class="side-menu__icon"> <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round" class="feather feather-navigation"><polygon points="3 11 22 2 13 21 11 13 3 11"></polygon></svg></div>
+                <div class="side-menu__icon"> <i data-feather="home"></i> </div>
                 <div class="side-menu__title">Logout  </div>
             </a>
             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
@@ -124,8 +122,7 @@
 
 
 <!-- BEGIN: JS Assets-->
-<script src="https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/markerclusterer.js"></script>
-<script src="https://maps.googleapis.com/maps/api/js?key=["your-google-map-api"]&libraries=places"></script>
+
 <!-- END: JS Assets-->
 @yield('jsContent')
 </body>
